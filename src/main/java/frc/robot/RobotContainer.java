e2814dcf40d91dec3b3765001bc4ac84f9192069
Darren Seubert <dpseubert15@gmail.com>
2022-01-11 10:29:33 -0600

    Removed all pneumatic devices

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,16 +6,12 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.ClimberSetPercentOutput;
 import frc.robot.commands.DrivetrainArcadeDrive;
-import frc.robot.commands.DrivetrainShiftingSetState;
 import frc.robot.commands.IntakeBottomMotorSetPercentOutput;
-import frc.robot.commands.IntakePistonsSetState;
 import frc.robot.commands.IntakeTopMotorSetPercentOutput;
 import frc.robot.commands.ShooterSetPercentOutput;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.DrivetrainShifting;
 import frc.robot.subsystems.IntakeBottomMotor;
-import frc.robot.subsystems.IntakePistons;
 import frc.robot.subsystems.IntakeTopMotor;
 import frc.robot.subsystems.Shooter;
 
@@ -25,19 +21,15 @@ public class RobotContainer {
 
     private final Climber mClimber = new Climber();
 	private final Drivetrain mDrivetrain = new Drivetrain();
-	private final DrivetrainShifting mDrivetrainShifting = new DrivetrainShifting();
     private final IntakeTopMotor mIntakeTopMotor = new IntakeTopMotor();
-    private final IntakePistons mIntakePistons = new IntakePistons();
     private final IntakeBottomMotor mIntakeBottomMotor = new IntakeBottomMotor();
     private final Shooter mShooter = new Shooter();
 
 	public RobotContainer() {
+        mIntakeTopMotor.setDefaultCommand(new IntakeTopMotorSetPercentOutput(mIntakeTopMotor, 0.0));
         mClimber.setDefaultCommand(new ClimberSetPercentOutput(mClimber, 0.0));
 		mDrivetrain.setDefaultCommand(new DrivetrainArcadeDrive(mDrivetrain, mXbox));
-		mDrivetrainShifting.setDefaultCommand(new DrivetrainShiftingSetState(mDrivetrainShifting, true));
         mIntakeBottomMotor.setDefaultCommand(new IntakeBottomMotorSetPercentOutput(mIntakeBottomMotor, 0.0));
-        mIntakePistons.setDefaultCommand(new IntakePistonsSetState(mIntakePistons, false));
-        mIntakeTopMotor.setDefaultCommand(new IntakeTopMotorSetPercentOutput(mIntakeTopMotor, 0.0));
         mShooter.setDefaultCommand(new ShooterSetPercentOutput(mShooter, 0.0));
 
 		configureButtonBindings();
@@ -71,15 +63,11 @@ public class RobotContainer {
 		jButton11 = new JoystickButton(mJoystick, 11);
 		jButton12 = new JoystickButton(mJoystick, 12);
 
-		xButtonA.whenPressed(new DrivetrainShiftingSetState(mDrivetrainShifting, false));
-		xButtonB.whenPressed(new DrivetrainShiftingSetState(mDrivetrainShifting, true));
-        xButtonY.whenPressed(new IntakePistonsSetState(mIntakePistons, true));
-        xButtonX.whenPressed(new IntakePistonsSetState(mIntakePistons, false));
-
         jButton1.whileHeld(new ShooterSetPercentOutput(mShooter, 1.0));
-        jButton2.whileHeld(new IntakeTopMotorSetPercentOutput(mIntakeTopMotor, 1.0));
         jButton3.whileHeld(new IntakeBottomMotorSetPercentOutput(mIntakeBottomMotor, 1.0));
         jButton4.whileHeld(new IntakeBottomMotorSetPercentOutput(mIntakeBottomMotor, -1.0));
+
+        jButton2.whileHeld(new IntakeTopMotorSetPercentOutput(mIntakeTopMotor, 1.0));
 
         jButton7.whileHeld(new ClimberSetPercentOutput(mClimber, 1.0));
         jButton8.whileHeld(new ClimberSetPercentOutput(mClimber, -1.0));
